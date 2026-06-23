bb80dc1711a1c60a677eb7eaf2d3e06de46b28ff
Samba <danielsamba0000@icloud.com>
2026-06-23 08:24:06 +0100

    first commit

---
+++ src/main/java/E_Commerce/Master/MasterApplication.java
@@ -0,0 +1,13 @@
+package E_Commerce.Master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterApplication.class, args);
+	}
+
+}
